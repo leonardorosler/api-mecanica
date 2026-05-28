1d57b96911c25329c87a71886296b5e1b589cb1d
Leonardo Rosler <roslerleonardo@gmail.com>
2026-05-27 22:40:48 -0300

    feat: adiciona estrutura inicial ao projeto

---
+++ src/routes/server.ts
@@ -0,0 +1,11 @@
+import express from "express"
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send("Teste 001")
+})
+
+app.listen(port, () => {console.log(`Servidor rodando na porta: ${port}`)})
