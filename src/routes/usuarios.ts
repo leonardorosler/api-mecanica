97decae0a055ed08a993c402ade6f8308af4a3ca
Leonardo Rosler <roslerleonardo@gmail.com>
2026-06-27 21:30:18 -0300

    feat: valida e criptografa senha de usuário

--- src/routes/usuarios.ts
+++ src/routes/usuarios.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { prisma } from "../../lib/prisma";
 import z from "zod";
+import bcrypt from "bcryptjs";
 
 const router = Router();
 
 const usuarioSchema = z.object({
   nome: z.string().min(2),
   email: z.string().email(),
-  senha: z.string().min(1),
+  senha: z.string()
+    .min(8, "A senha deve ter no minimo 8 caracteres")
+    .regex(/[a-z]/, "A senha deve ter pelo menos uma letra minuscula")
+    .regex(/[A-Z]/, "A senha deve ter pelo menos uma letra maiuscula")
+    .regex(/[0-9]/, "A senha deve ter pelo menos um numero")
+    .regex(/[^A-Za-z0-9]/, "A senha deve ter pelo menos um simbolo"),
   nivel: z.number().int().min(1).max(3).optional()
 });
 
@@ -37,8 +43,13 @@ router.post("/", async (req, res) => {
     const valida = usuarioSchema.safeParse(req.body);
     if (!valida.success) return res.status(400).json({ erro: valida.error });
 
+    const senhaCriptografada = await bcrypt.hash(valida.data.senha, 10);
+
     const usuario = await prisma.usuario.create({
-      data: valida.data,
+      data: {
+        ...valida.data,
+        senha: senhaCriptografada
+      },
       select: {
         id: true,
         nome: true,
